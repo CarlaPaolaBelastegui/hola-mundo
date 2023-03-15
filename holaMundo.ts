67d2d770ada8867736d1c469e216eba48bbdc7c4
carla belastegui <carlabelastegui@gmail.com>
2023-03-15 18:45:39 -0300

    programacion

--- holaMundo.ts
+++ holaMundo.ts
@@ -1,1 +1,5 @@
 console.log ("hola mundo!");
+console.log( "descargar more - fondo .. de pantalla para pc");
+ console.log("entrar a google y buscar en el buscador");
+console.log("insertar lo que se busca");
+ console.log("descargar la imagen deseada");
